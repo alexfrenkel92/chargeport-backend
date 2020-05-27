1b77fdc720e7c26b6aece87752d9cfbddd273f33
alexfrenkel92 <alexfrenkel92@gmail.com>
2020-05-27 18:44:49 +0200

    email sender works, endpoint ok

---
+++ src/app/services/subscriptionService.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const repository = require('../repositories/subscriptionRepository');
+
+async function postSubscriptionService () {
+    try {
+        const result = await repository.postSubscriptionRepository();
+        return result;
+    } catch (error) {
+        console.log(`Service erro: ${error}`);
+    }
+}
+
+async function deleteSubscriptionService (id) {
+    try {
+        const result = await repository.deleteSubscriptionRepository(id);
+        return result;
+    } catch (error) {
+        console.log(`Service erro: ${error}`);
+    }
+}
+
+module.exports = {
+    postSubscriptionService: postSubscriptionService,
+    deleteSubscriptionService: deleteSubscriptionService
+}
