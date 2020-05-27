072182730f854e10e584c18a13292e1e97b5b568
alexfrenkel92 <alexfrenkel92@gmail.com>
2020-05-27 19:22:44 +0200

    email sender connected with frontend

--- src/app/services/emailService.js
+++ src/app/services/emailService.js
@@ -4,31 +4,31 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 const nodemailer = require('nodemailer');
 
-async function emailSender(name, email) {
-    const transporter = nodemailer.createTransport({
-        service: process.env.EMAIL_SERVICE,
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASSWORD
-        }
-    });
+async function emailSender (name, email) {
+  const transporter = nodemailer.createTransport({
+    service: process.env.EMAIL_SERVICE,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD
+    }
+  });
 
-    const mailOptions = {
-        from: 'alex.frenkel92@gmail.com',
-        to: `${email}`,
-        subject: 'Chargeport',
-        text: `Dear ${name}, \n\nThanks for trying out this service!`
-    };
+  const mailOptions = {
+    from: 'alex.frenkel92@gmail.com',
+    to: `${email}`,
+    subject: 'Chargeport',
+    text: `Dear ${name}, \n\nThanks for trying out this service!`
+  };
 
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    })
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
 }
 
 module.exports = {
-    emailSender: emailSender
-}
+  emailSender: emailSender
+};
--- src/app/services/subscriptionService.js
+++ src/app/services/subscriptionService.js
@@ -3,24 +3,24 @@
 const repository = require('../repositories/subscriptionRepository');
 
 async function postSubscriptionService () {
-    try {
-        const result = await repository.postSubscriptionRepository();
-        return result;
-    } catch (error) {
-        console.log(`Service erro: ${error}`);
-    }
+  try {
+    const result = await repository.postSubscriptionRepository();
+    return result;
+  } catch (error) {
+    console.log(`Service erro: ${error}`);
+  }
 }
 
 async function deleteSubscriptionService (id) {
-    try {
-        const result = await repository.deleteSubscriptionRepository(id);
-        return result;
-    } catch (error) {
-        console.log(`Service erro: ${error}`);
-    }
+  try {
+    const result = await repository.deleteSubscriptionRepository(id);
+    return result;
+  } catch (error) {
+    console.log(`Service erro: ${error}`);
+  }
 }
 
 module.exports = {
-    postSubscriptionService: postSubscriptionService,
-    deleteSubscriptionService: deleteSubscriptionService
-}
+  postSubscriptionService: postSubscriptionService,
+  deleteSubscriptionService: deleteSubscriptionService
+};
