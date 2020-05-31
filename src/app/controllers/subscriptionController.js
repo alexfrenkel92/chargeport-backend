70add007835f990dc7fad466747d14387603efd8
alexfrenkel92 <alexfrenkel92@gmail.com>
2020-05-31 21:24:57 +0200

    consumption ok

--- src/app/controllers/subscriptionController.js
+++ src/app/controllers/subscriptionController.js
@@ -3,14 +3,22 @@
 const express = require('express');
 const router = express.Router();
 const service = require('../services/subscriptionService');
+const emailSender = require('../services/emailService');
 
 router.post('/', async (req, res) => {
-    const subscriber_name = req.body.name; //E-mail service gonna use this
-    const subscriber_emial = req.body.email; //E-mail service gonna use this
+    const name = req.body.name; //E-mail service gonna use this
+    const email = req.body.email; //E-mail service gonna use this
+
+    if (name === '' || email === '') {
+        res.status(400).json({ status: 'Missing fields' });
+        return;
+    }
+
     try {
         const subscription = await service.postSubscriptionService();
-        res.status(200);
-        res.send(subscription);
+        const subscriptionCorfirmation = await emailSender.emailSender(name, email);
+        res.status(201).send(subscription);
+        res.status(200).send(subscriptionCorfirmation);
     } catch (error) {
         console.log(`Controller error: ${error}`);
         return res.status(400);
