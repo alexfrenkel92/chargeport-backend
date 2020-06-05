c058ed74b02635facbddf28bee9fbdc0d8035912
alexfrenkel92 <alexfrenkel92@gmail.com>
2020-06-05 21:26:52 +0200

    port corrected

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const app = require('./app/routes.js');
-const PORT = 8080;
+// const PORT = 8080;
+const PORT = 'https://chargeport-backend.herokuapp.com/'
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
