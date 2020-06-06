31c54b4fe6a6c2b4f0bc47ab09449cd85101886c
alexfrenkel92 <alexfrenkel92@gmail.com>
2020-06-06 04:51:25 +0200

    email attempt3

--- src/app/services/emailService.js
+++ src/app/services/emailService.js
@@ -8,8 +8,6 @@ async function emailSender(name, email) {
   const smtpTransport = require('nodemailer-smtp-transport');
   const transporter = nodemailer.createTransport(smtpTransport({
     service: process.env.EMAIL_SERVICE,
-    port: 465,
-    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD
